b2e3aafadf1fcdce989854045a108c1231412dd5
Joe Pringle <pringj3@rpi.edu>
2015-12-29 17:45:08 -0500

    initial commit. going to delete old repo and restructure for django

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jpringle/Documents/Github/sc2-barcode-scanner/env/bin/python3.4
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
