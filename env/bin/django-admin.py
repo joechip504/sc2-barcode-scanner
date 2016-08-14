768891840f5002a22149ebb4c1f53a55bbbfbed3
Joe Pringle <pringj3@rpi.edu>
2016-08-14 16:43:35 -0400

    remove venv (using docker instead)

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jpringle/Documents/Github/sc2-barcode-scanner/env/bin/python3.4
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
